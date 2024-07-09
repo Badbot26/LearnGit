7f2f8e5e05cfa70035b873319fad97908960e102
Wayne Michael <pwaynemichael@gmail.com>
2024-07-09 20:50:21 +0200

    feat: added message to the console.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Example of a message displayed to the console.");
         }
     }
 }
