cb140555009f37fc06ba9bee520b123c2c76ec89
Wayne Michael <pwaynemichael@gmail.com>
2024-07-09 20:56:18 +0200

    feat: added second message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Example of a message displayed to the console.");
+            Console.WriteLine("Second message.");
         }
     }
 }
